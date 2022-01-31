e287a549b1a0de64ded4f4c5d87062b0df3c2216
eden.ju <public.jin03@gmail.com>
2022-01-31 20:11:25 +0900

    feat: 계산기 테스트 코드 추가

---
+++ src/test/calculator.test.js
@@ -0,0 +1,60 @@
+const Calculator = require("../calculator.js");
+
+describe("Calculator", () => {
+  let cal;
+  beforeEach(() => {
+    cal = new Calculator();
+  });
+
+  it("inits with 0", () => {
+    expect(cal.value).toBe(0);
+  });
+
+  it("sets the value", () => {
+    cal.set(3);
+    expect(cal.value).toBe(3);
+  });
+
+  it("clears the value", () => {
+    cal.set(3);
+    cal.clear();
+    expect(cal.value).toBe(0);
+  });
+
+  it("add case", () => {
+    cal.set(1);
+    cal.add(2);
+    expect(cal.value).toBe(3);
+  });
+
+  it("subtract case", () => {
+    cal.set(3);
+    cal.subtract(2);
+    expect(cal.value).toBe(1);
+  });
+
+  it("multiply case", () => {
+    cal.set(1);
+    cal.multiply(4);
+    expect(cal.value).toBe(4);
+  });
+
+  describe("divides", () => {
+    it("0 / 0 === NaN", () => {
+      cal.divide(0);
+      expect(cal.value).toBe(NaN);
+    });
+
+    it("1 / 0 === Infinity", () => {
+      cal.set(1);
+      cal.divide(0);
+      expect(cal.value).toBe(Infinity);
+    });
+
+    it("4 / 4 === 1", () => {
+      cal.set(4);
+      cal.divide(4);
+      expect(cal.value).toBe(1);
+    });
+  });
+});
